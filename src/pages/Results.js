901f75e3718ad02ea34c259973680b860fbd7b50
adam.sernheim <adam@sernheim.com>
2020-10-27 14:57:15 +0100

    Fixed distance

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import results from '../../data/results.json';
+import Distance from '../components/Distance';
 
 const IndexPage = () => (
   <Layout fullMenu>
