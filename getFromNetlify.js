5ec40d183854d35918c84149cff26b50ac8311d8
adam.sernheim <adam@sernheim.com>
2020-10-27 14:47:44 +0100

    modify build script

--- getFromNetlify.js
+++ getFromNetlify.js
@@ -6,6 +6,11 @@ if (process.env.NETLIFY_SIGNUP_FORM_ID === undefined) {
   done();
   return;
 }
+if (process.env.NETLIFY_RESULT_FORM_ID === undefined) {
+  console.log('Missing NETLIFY_RESULT_FORM_ID environment variable!');
+  done();
+  return;
+}
 
 if (process.env.NETLIFY_API_AUTH === undefined) {
   console.log('Missing NETLIFY_API_AUTH environment variable!');
@@ -14,6 +19,7 @@ if (process.env.NETLIFY_API_AUTH === undefined) {
 }
 
 let registrationUrl = `https://api.netlify.com/api/v1/forms/${process.env.NETLIFY_SIGNUP_FORM_ID}/submissions/?access_token=${process.env.NETLIFY_API_AUTH}`;
+let resultsUrl = `https://api.netlify.com/api/v1/forms/${process.env.NETLIFY_RESULT_FORM_ID}/submissions/?access_token=${process.env.NETLIFY_API_AUTH}`;
 
 const buildDest = '.';
 
@@ -52,6 +58,54 @@ const buildDest = '.';
                       console.log(err);
                     } else {
                       console.log('Sign up data saved.');
+                    }
+                  }
+                );
+              }
+            });
+          }
+        });
+    })
+    .catch(err => console.log(err));
+})();
+
+
+(async () => {
+  await fetch(resultsUrl)
+    .then(res => res.json())
+    .then(json => {
+        let results = [];
+
+        for (let item of json) {
+          let result = {
+            name: item.data.name,
+            distance: item.data.distance,
+            time: item.data.time,
+            link: item.data.link
+          };
+
+          results.push(result);
+        }
+
+        // write data to file
+        fs.mkdir(buildDest, function(err) {
+          if (err && err.code !== 'EEXIST') {
+            console.log(err);
+            done();
+          } else {
+            fs.mkdir(buildDest + '/data', function(err) {
+              if (err && err.code !== 'EEXIST') {
+                console.log(err);
+                done();
+              } else {
+                fs.writeFile(
+                  buildDest + '/data/results.json',
+                  JSON.stringify(results, null, 2),
+                  function(err) {
+                    if (err) {
+                      console.log(err);
+                    } else {
+                      console.log('Result data saved.');
                     }
                   }
                 );
