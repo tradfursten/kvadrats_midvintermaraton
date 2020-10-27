eaac1b778dbab3e69fdc3b870469b3557e307d15
adam.sernheim <adam@sernheim.com>
2020-10-27 07:32:13 +0100

    Registreration forms

---
+++ src/pages/SignUp_success.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout fullMenu>
+    <article id="main">
+      <header>
+        <h2>Tack för din anmälan!</h2>
+        <p>Vi kontaktar dig när loppet startar!</p>
+      </header>
+    </article>
+  </Layout>
+);
+
+export default IndexPage;
