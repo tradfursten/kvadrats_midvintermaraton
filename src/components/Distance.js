14920e3b068c64ace323408c2e80c32ba4470ab7
adam.sernheim <adam@sernheim.com>
2020-11-09 09:11:04 +0100

    Fixed distance component

--- src/components/Distance.js
+++ src/components/Distance.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Distance({d}) {
+export default function Distance({ d }) {
   let distance = '';
   switch (d) {
     case '42k':
@@ -9,9 +9,15 @@ export default function Distance({d}) {
     case '21k':
       distance = 'Halvmarathon';
       break;
+    case '10k':
+      distance = '10 kilometer';
+      break;
+    case '5k':
+      distance = '5 kilometer';
+      break;
     default:
       distance = d;
   }
 
-  return <span>{distance}</span>
+  return <span>{distance}</span>;
 }
